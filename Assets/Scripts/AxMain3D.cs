438da8e29c2dd88822f10f3e8f1cff54b0f23315
jgsampson <js8323@nyu.edu>
2017-04-22 21:41:19 -0400

    Playable build

--- Assets/Scripts/AxMain3D.cs
+++ Assets/Scripts/AxMain3D.cs
@@ -12,37 +12,39 @@ public class AxMain3D : MonoBehaviour {
 	public GameObject TankCamera;
 	public GameObject FaceGroup;
 	public GameObject AllDays;
+	public GameObject FadeSquare;
 	
 	public Material TankMat;
 	public RenderTexture rTexture;
 	public GameObject TankQuad;
 
-    static List<string> templateDB = new List<string>();
-    static List<string> phraseDB = new List<string>();
     List<string> todaysTemplate; 
     List<string> todaysPhrases;
     List<string> completedText;
-    List<string> currentEntries;
     List<string>[] dayText = new List<string>[10];
     Day[] allDays = new Day[10];
     GameObject[] dayObjs = new GameObject[10];
-    int zMove = 0, currentDay = 1, maxDays = 10, textState = 0, currentPhrase = 0, currentIndex = 1; 
+    int zMove = 0, currentDay = 1, maxDays = 10, textState = -1, currentPhrase = 0, currentIndex = 1; 
 	float lastShowDay = 0.0f;
-	string currentEntry = " ", currentPhrases = "", currentTemplate = "";
-	bool releaseTyping = false, templateComplete = false, phraseMatch = false;
+	Color guiColor = Color.grey;
+	string currentEntry = " ", currentPhrases = "", currentTemplate = "", gameState = "intro"; // gameState = intro, startDay, active, endDay
+	bool releaseTyping = false, templateComplete = false, phraseMismatch = false;
+	
 
 	public class Day {
 		public List<string> dayTemplates;
 		public List<string> dayPhrases;
 
-		public Day(int[] temps, int[] phrases) { // constructor
+		public Day(string tmp, string phs) { // constructor
 			dayTemplates = new List<string>();
-			for (int i=0; i<temps.Length; i++) 
-				this.dayTemplates.Add(templateDB[temps[i]]);
+			this.dayTemplates.Add(tmp);
+			//for (int i=0; i<temps.Length; i++) 
+				//this.dayTemplates.Add(templateDB[temps[i]]);
 
 			dayPhrases = new List<string>();
-			for (int i=0; i<phrases.Length; i++) 
-				this.dayPhrases.Add(phraseDB[phrases[i]]);
+			string[] psp = phs.Split(',');
+			for (int i=0; i<psp.Length; i++) 
+				this.dayPhrases.Add(psp[i]);
 		}
 
 		public void DebugDay() {
@@ -69,18 +71,21 @@ public class AxMain3D : MonoBehaviour {
     	}
     }
 
-    void ReadFile(string fn, List<string> outlist) {
+    void ReadFile(string fn, string[] ar) {
     	try {
 			StreamReader fr = new StreamReader(fn);
 			string line;
-
+			int ln = 0;
 			using (fr)
 	         {
 	             do
 	             {
 	                 line = fr.ReadLine();
-	                 if (line != null)
-	               		outlist.Add(line);
+	                 if (line != null) {
+	                 	ar[ln] = line;
+	                 	ln++;
+	                 }
+	               		
 	              }
 	             while (line != null);
 	          
@@ -101,7 +106,6 @@ public class AxMain3D : MonoBehaviour {
     }
 
     void UpdateStrings() {
-    	// TODO compare for what's been typed and color accordingly, for now, just build 
 		currentPhrases = "";
 		currentTemplate = "";
 
@@ -113,7 +117,10 @@ public class AxMain3D : MonoBehaviour {
 			if (i % 2 != 0) {
 				todaysTemplate[i] = todaysTemplate[i].ToLower();
 				if (i == currentIndex && textState == 1 && !releaseTyping) { // this is what we're editing
-				 	currentTemplate += "[<color=#FFFF00>" + todaysTemplate[i] + "</color>]";
+					if (phraseMismatch) // trying to lock something in that's not an available phrase
+				 		currentTemplate += "[<color=#FF0000>" + todaysTemplate[i] + "</color>]";
+				 	else
+				 		currentTemplate += "[<color=#FFFF00>" + todaysTemplate[i] + "</color>]";
 				}
 				else 
 					currentTemplate += todaysTemplate[i];
@@ -125,7 +132,20 @@ public class AxMain3D : MonoBehaviour {
 
     }
 
+    bool CheckPhraseMatch() {
+    	//return false;
+    	bool foundMatch = false;
+    	for (int i=0; i<allDays[(currentDay-1)].dayPhrases.Count; i++) {
+			if (allDays[(currentDay-1)].dayPhrases[i] == todaysTemplate[currentIndex]) {
+				foundMatch = true;
+				break;
+			}
+		}
+		return foundMatch;
+    }
+
     void EnterText(KeyCode ltr) {
+    	phraseMismatch = false;
     	if (ltr >= KeyCode.A && ltr <= KeyCode.Z) { // we only accept alphabetical characters
     		todaysTemplate[currentIndex] += ltr;
     	}
@@ -137,12 +157,25 @@ public class AxMain3D : MonoBehaviour {
     			releaseTyping = true;
     			todaysTemplate[currentIndex] = "___";
     		}
-	   		else { // lock text in
-	    		releaseTyping = true;
-	    		currentIndex += 2;
-	    		if (currentIndex >= todaysTemplate.Count) {
-	    			templateComplete = true;
-	    		}
+	   		else { // attempt to lock text in
+	   			if (CheckPhraseMatch()) {
+	   				
+	    			for (int i=0; i<allDays[(currentDay-1)].dayPhrases.Count; i++) {
+						if (allDays[(currentDay-1)].dayPhrases[i] == todaysTemplate[currentIndex]) {
+							allDays[(currentDay-1)].dayPhrases.RemoveAt(i);
+							break;
+						}
+					}
+					
+					releaseTyping = true;
+	    			currentIndex += 2;
+	    			if (currentIndex >= todaysTemplate.Count) {
+	    				templateComplete = true;
+	    			}
+	   			}
+	   			else
+	   				phraseMismatch = true;
+	    		
     		}
     		
     	}
@@ -152,10 +185,12 @@ public class AxMain3D : MonoBehaviour {
 	void NewDayUpdate() {
 		todaysTemplate = new List<string>();
     	todaysPhrases = new List<string>();
-		currentEntries = new List<string>();
+		
 		string curStr = "";
 		int numEntries = 0;
+		currentIndex = 1;
 		templateComplete = false;
+		textState = -1;
 		for (int i=0; i<allDays[(currentDay-1)].dayTemplates.Count; i++) { // count the number of phrases in template
 			for (int j=0; j<allDays[(currentDay-1)].dayTemplates[i].Length;j++) {
 				if (allDays[(currentDay-1)].dayTemplates[i][j] == '_') {
@@ -171,7 +206,6 @@ public class AxMain3D : MonoBehaviour {
 					todaysTemplate.Add(curStr);
 			}
 		}
-
 
 		// Change alpha of tankQuad
 		Color color = TankMat.color;
@@ -219,49 +253,92 @@ public class AxMain3D : MonoBehaviour {
 			tmpObj.SetActive(false);
 		}
 
+		string[] allTmps = new string[10];
+		string[] allPhrases = new string[10];
 
 		// populate the template and phrase  arrays
-		ReadFile("template.txt", templateDB);
-	    ReadFile("phrase.txt", phraseDB);
+		ReadFile("template.txt", allTmps);
+	    ReadFile("phrase.txt", allPhrases);
 		
 	    // Set up the text for Days
-	    	// manual
-	    //			  array of templates    array of phrases
-	  	allDays[0] = new Day(new int[] {0},new int[]{1,3});
-	  	allDays[1] = new Day(new int[] {1},new int[]{2,4,5});
-	  	allDays[2] = new Day(new int[] {0},new int[]{2,4,5});
-	  	allDays[3] = new Day(new int[] {1},new int[]{2,4,5});
-	  	allDays[4] = new Day(new int[] {0},new int[]{2,4,5});
-	  	allDays[5] = new Day(new int[] {1},new int[]{2,4,5});
-	  	allDays[6] = new Day(new int[] {0},new int[]{2,4,5});
-	  	allDays[7] = new Day(new int[] {0},new int[]{2,4,5});
-	  	allDays[8] = new Day(new int[] {0},new int[]{2,4,5});
-	  	allDays[9] = new Day(new int[] {0},new int[]{2,4,5});
-	
+	    for (int i=0;i<10;i++) 
+	    	allDays[i] = new Day(allTmps[i],allPhrases[i]);
 		NewDayUpdate();
 		
 	}	
 
 	void OnGUI() {
-		if (Time.time - lastShowDay < 2)
-			GUI.Label(new Rect(Screen.width-100, Screen.height-20, 100, 20), "Current Day: " + currentDay.ToString());	
+		GUI.color = guiColor;
+		if (gameState == "intro") {
+			GUI.skin.label.fontSize = 18;
+			GUI.Label(new Rect((Screen.width/4), Screen.height-300, 600, 300), "I don't quite remember when it started, but I began to see this strange thing every day...");	
 
-		// show current day template
-		GUI.skin.label.fontSize = 18;
-		GUI.Label(new Rect((Screen.width/3)-currentTemplate.Length, Screen.height-50, 500, 200), currentTemplate);	
+			GUI.skin.label.fontSize = 15;
+			GUI.Label(new Rect(Screen.width-250, Screen.height-30, 200, 200), "(Press E to begin)");	
+		}
+		else if (gameState == "outro") {
+			GUI.skin.label.fontSize = 18;
+			GUI.Label(new Rect((Screen.width/2), Screen.height-300, 600, 200), "It walked out of the aquarium one final time, and I never saw it again...");	
 
-		GUI.skin.label.fontSize = 14;
-		// show available phrases   currentPhrases
-		GUI.Label(new Rect(Screen.width-100, 100, 100, 600), currentPhrases);	
+			GUI.skin.label.fontSize = 15;
+			GUI.Label(new Rect(Screen.width-250, Screen.height-30, 200, 200), "(Press ESC to exit)");	
+		}
+		else {
 
-		if (textState == 1) { // we're typing
-			Event e = Event.current;
-        	if (e.isKey && e.type == EventType.KeyUp) // key up
-        		EnterText(e.keyCode);
+			if (Time.time - lastShowDay < 2 && gameState == "active") {
+				GUI.skin.label.fontSize = 14;
+				GUI.Label(new Rect(Screen.width-120, 10, 120, 20), "Current Day: " + currentDay.ToString());	
+			}
+
+			if (textState > -1) {
+				// show current day template
+				GUI.skin.label.fontSize = 18;
+				GUI.Label(new Rect((Screen.width/3)-currentTemplate.Length, Screen.height-75, 600, 300), currentTemplate);	
+
+				// show available phrases 
+				GUI.skin.label.fontSize = 14;
+				GUI.Label(new Rect(Screen.width-100, Screen.height-100, 100, 600), currentPhrases);	
+
+				if (textState == 1) { // we're typing
+					Event e = Event.current;
+		        	if (e.isKey && e.type == EventType.KeyUp) // key up
+		        		EnterText(e.keyCode);
+				}
+				
+				GUI.skin.label.fontSize = 15;
+				if (templateComplete) 
+					GUI.Label(new Rect(Screen.width-250, Screen.height-30, 200, 200), "(Press E to complete writing)");	
+				else if (textState == 1)
+					//GUI.Label(new Rect(Screen.width-250, Screen.height-30, 200, 200), "(Press Enter when finished)");	
+				;
+				else 
+					GUI.Label(new Rect(Screen.width-250, Screen.height-30, 200, 200), "(Press Enter to write)");	
+				
+
+			}
+			else if (textState == -1) {
+				GUI.skin.label.fontSize = 15;
+				GUI.Label(new Rect(Screen.width-250, Screen.height-30, 200, 200), "(Press E to begin writing)");	
+			}
 		}
 	}
 	
 	void Update () {
+		if (gameState == "startDay") {
+			guiColor = Color.Lerp(Color.black, Color.grey, Time.time);
+			if (FadeSquare.GetComponent<SpriteRenderer>().color.a > 0) {
+				FadeSquare.GetComponent<SpriteRenderer>().color = new Color(0f,0f,0f,FadeSquare.GetComponent<SpriteRenderer>().color.a-(float)0.02);
+			}
+			else
+				gameState = "active";
+		}
+		else if (gameState == "endDay") {
+			guiColor = Color.Lerp(Color.grey, Color.black, Time.time);
+		}
+		else if (gameState == "intro") {
+			guiColor = Color.grey;
+		}
+
 		// x-axis camera movement
 		if (Input.mousePosition.x > 0 && Input.mousePosition.x < (Screen.width-5)) { // only allow mouse movements that are within game window
 
@@ -294,14 +371,26 @@ public class AxMain3D : MonoBehaviour {
 				
 			
 		}
-		else if (zMove == 3) { // test rotation to left
-			if (MainCamera.transform.localRotation.y > -.7) 
-				MainCamera.transform.localRotation = new Quaternion(MainCamera.transform.localRotation.x, MainCamera.transform.localRotation.y-(float)0.05, MainCamera.transform.localRotation.z, MainCamera.transform.localRotation.w);	
-			else
-				zMove = 0;
+		else if (zMove == 3) { // fading scene out
+			if (FadeSquare.GetComponent<SpriteRenderer>().color.a < 1) {
+				//MainCamera.transform.localRotation = new Quaternion(MainCamera.transform.localRotation.x, MainCamera.transform.localRotation.y-(float)0.005, MainCamera.transform.localRotation.z, MainCamera.transform.localRotation.w);	
+				FadeSquare.GetComponent<SpriteRenderer>().color = new Color(0f,0f,0f,FadeSquare.GetComponent<SpriteRenderer>().color.a+(float)0.02);
+			}
+			else {
+				if (gameState != "outro") {
+					gameState = "startDay";
+					zMove = 0;
+					NewDayUpdate();	
+				}
+				
+			}
 		}
 
 		// Keystrokes
+
+		if (Input.GetKey("escape"))
+    		Application.Quit();
+
 		if (Input.GetKeyUp("return") && textState == 0 && !templateComplete) {
 			// entering typing mode
 			textState = 1;
@@ -309,33 +398,52 @@ public class AxMain3D : MonoBehaviour {
 			UpdateStrings();
 		}
 
- 		if (Input.GetKey("escape"))
-    		Application.Quit();
-
-    	if (textState == 0) { // only if we're not typing something
-	    	if (Input.GetKey("w") && zMove == 0 && textState == 0) {
+ 		
+		
+		if (Input.GetKeyUp("e") && textState == -1 && gameState == "active") { 
+	    	textState = 0;
+	    	UpdateStrings();
+    	}
+    	else if (Input.GetKeyUp("e") && gameState == "intro") { 
+	    	gameState = "startDay";
+    	}
+	    
+    	if (textState != 1) { // only if we're not typing something
+	    	if (Input.GetKey("w") && zMove == 0) {
 	    		SendAction("stepFoward");
 	    		zMove = 1;
 	    	}
-			if (Input.GetKey("s") && zMove == 2 && textState == 0) {
+			if (Input.GetKey("s") && zMove == 2) {
 	    		SendAction("stepBack");
 	    		zMove = -1;    		
 			}
-			if (Input.GetKeyUp("a")) { // DEBUG move day backward
-	    		currentDay = (currentDay == 1) ? (maxDays) : (currentDay - 1);
-	    		lastShowDay = Time.time;
-	    		NewDayUpdate();
-	    	}
-	    	if (Input.GetKeyUp("q")) {
-	    		MainCamera.GetComponent<Camera>().orthographic = false;
-	    		zMove = 3;
-	    	}
-			if (Input.GetKeyUp("d")) { // DEBUG move day forward
-	    		currentDay = (currentDay == maxDays) ? (1) : (currentDay + 1);    
-	    		lastShowDay = Time.time;
-	    		NewDayUpdate();
+	    	if (Input.GetKeyUp("e")) { 
+	    		if (templateComplete) {
+		    		zMove = 3;
+		    		currentDay = (currentDay == maxDays) ? (99) : (currentDay + 1);    
+		    		lastShowDay = Time.time;
+		    		if (currentDay == 99)
+		    			gameState = "outro";
+		    		else
+		    			gameState = "endDay";
+	    		}
 	    	}
 		}
+
+
+	    if (Input.GetKeyUp(",")) { // DEBUG move day backward
+			zMove = 3;
+			gameState = "endDay";
+    		currentDay = (currentDay == 1) ? (maxDays) : (currentDay - 1);
+    		lastShowDay = Time.time;
+	    }
+		if (Input.GetKeyUp(".")) { // DEBUG move day forward
+			zMove = 3;
+			gameState = "endDay";
+    		currentDay = (currentDay == maxDays) ? (1) : (currentDay + 1);    
+    		lastShowDay = Time.time;
+	    }
+	    
 		if (releaseTyping) {
 			textState = 0;
 			releaseTyping = false;
