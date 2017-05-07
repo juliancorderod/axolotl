09c53f100394af8ed54fb177e55ede33314575c6
Rahul Joshi <rahuljoshi@172-18-183-67.DYNAPOOL.NYU.EDU>
2017-05-06 23:23:03 -0400

    The smoking Scene clouds now loop seamlessly.

--- Assets/Scripts/smokeSceneAnim.cs
+++ Assets/Scripts/smokeSceneAnim.cs
@@ -8,13 +8,22 @@ public class smokeSceneAnim : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+		clouds.transform.localPosition = new Vector3 (
+			-27.5f,
+			clouds.transform.localPosition.y,
+			clouds.transform.localPosition.z);
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if (clouds.transform.localPosition.x >= 36.84f) {
+			clouds.transform.localPosition = new Vector3 (
+				-27.5f,
+				clouds.transform.localPosition.y,
+				clouds.transform.localPosition.z);
+		}
 
-		clouds.transform.localPosition += new Vector3(0.07f * Time.deltaTime, 0f,0f);
+		clouds.transform.localPosition += new Vector3 (0.07f * Time.deltaTime, 0f, 0f);
 
 		branches.transform.localEulerAngles = new Vector3(branches.transform.localEulerAngles.x, 
 			branches.transform.localEulerAngles.y, (Mathf.Sin(Time.time/10))*7);
