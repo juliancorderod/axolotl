6de5c398becdd2e1317cbe8502e9146e13837026
Rahul Joshi <rahuljoshi@172-18-183-67.DYNAPOOL.NYU.EDU>
2017-05-07 02:25:33 -0400

    Set up the aquarium (inside the tank) background and I’ve implemented the script that does all the fade in/out. Just drag and drop the prefab into the hierarchy

---
+++ Assets/Scripts/BackgroundFadeIn.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundFadeIn : MonoBehaviour {
+
+	AxMain3D am;
+	bool previousCloseToGlass;
+	float lerpTime = 0;
+
+	SpriteRenderer[] backgroundSpriteRenderers;
+	List<float> alphas = new List<float>();
+
+	// Use this for initialization
+	void Start () {
+		am = GameObject.Find ("AxMain").GetComponent<AxMain3D> ();
+		previousCloseToGlass = am.closeToGlass;
+
+		backgroundSpriteRenderers = GetComponentsInChildren<SpriteRenderer> ();
+
+		for (int i = 0; i < backgroundSpriteRenderers.Length; i++) {
+			alphas.Add (backgroundSpriteRenderers [i].color.a);
+			backgroundSpriteRenderers [i].color = new Color (1f, 1f, 1f, 0.1f);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (previousCloseToGlass != am.closeToGlass) {
+			if (am.closeToGlass == true) {
+				lerpTime += Time.deltaTime / 5f;
+				for (int i = 0; i < backgroundSpriteRenderers.Length; i++) {
+					backgroundSpriteRenderers [i].color =
+						Color.Lerp (
+							new Color (1f, 1f, 1f, alphas [i] * 0.1f),
+							new Color (1f, 1f, 1f, alphas [i]),
+							lerpTime);
+				}
+			} else if (am.closeToGlass == false) {
+				lerpTime += Time.deltaTime / 1f;
+				for (int i = 0; i < backgroundSpriteRenderers.Length; i++) {
+					backgroundSpriteRenderers [i].color =
+						Color.Lerp (
+							new Color (1f, 1f, 1f, alphas [i]),
+							new Color (1f, 1f, 1f, alphas [i] * 0.1f),
+							lerpTime);
+				}
+			}
+		} else {
+			lerpTime = 0f;
+		}
+
+		if (lerpTime >= 1f) {
+			previousCloseToGlass = am.closeToGlass;
+		}
+	}
+}
